73ba1d76c658454c41d5be140c02543ac36fe023
devJeremie <chabanaisjeremie@gmail.com>
2025-10-08 15:03:56 +0200

    Start-Store

---
+++ frontend/lib/api.ts
@@ -0,0 +1,34 @@
+import { Product } from "@/type";
+
+const API_URL = "https://fakestoreapi.com";
+
+//All products
+const getProducts = async (): Promise<Product[]> => {
+    try {
+       const response = await fetch(`${API_URL}/products`);
+       if (!response.ok) {
+        throw new Error('Network response was not ok');
+       }
+       return await response.json();
+    } catch (error) {
+        console.log('Error fetching products:', error);
+        throw error;
+    }
+};
+
+//All Categories
+const getCategories = async (): Promise<string[]> => {
+    try {
+       const response = await fetch(`${API_URL}/products/categories`);
+       if (!response.ok) {
+        throw new Error('Network response was not ok');
+       }
+       return await response.json();
+    } catch (error) {
+        console.log('Error fetching products:', error);
+        throw error;
+    }
+};
+
+export { getProducts, getCategories };
+
