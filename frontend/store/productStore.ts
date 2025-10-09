b181b55e7533608e9a306a56788e9f3b27dd88b0
devJeremie <chabanaisjeremie@gmail.com>
2025-10-09 11:48:47 +0200

    Maj-ProductSTore

--- frontend/store/productStore.ts
+++ frontend/store/productStore.ts
@@ -1,3 +1,4 @@
+import { getCategories, getProducts } from '@/lib/api';
 import { Product } from '@/type';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
@@ -5,14 +6,49 @@ import { createJSONStorage, persist } from 'zustand/middleware';
 
 interface ProductsState {
     products: Product[];
+    filteredProducts: Product[];
     categories: string[];
+    loading: boolean;
+    error: string | null;
+//Action to fetch products
+    fetchProducts: () => Promise<void>;
+    fetchCategories: () => Promise<void>;
 }
 
 export const useProductStore = create<ProductsState>()(
-    persist((set, get)=>({
+    persist(
+        (set, get)=>({
         products: [],
+        filteredProducts: [],
         categories: [],
-    }), {
+        loading: false,
+        error: null,
+
+        fetchProducts: async () => {
+           try {
+                set({ loading: true, error: null });
+                const products = await getProducts();
+                set({ 
+                    products,
+                    filteredProducts: products,
+                    loading: false 
+                });
+               
+           } catch (error: any) {
+                set({ error: error.message, loading: false });
+           }
+        },
+        fetchCategories: async () => {
+            try {
+               set({loading: true, error: null });
+               const categories = await getCategories();
+                set({ categories, loading: false });
+            } catch (error: any) {
+                set({ error: error.message, loading: false });
+            }
+        },
+    }), 
+    {
     name: 'product-storage',
     storage: createJSONStorage(() => AsyncStorage),
     })
