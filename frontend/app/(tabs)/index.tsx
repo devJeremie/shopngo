e03869383d2eb8d5616cd50d428e58a704b1a0cd
devJeremie <chabanaisjeremie@gmail.com>
2025-10-09 14:20:57 +0200

    Comment-index.tsx

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,13 +1,38 @@
 import HomeHeader from '@/components/HomeHeader';
 import { AppColors } from '@/constants/theme';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Product } from '@/type';
+import { useProductStore } from '@/store/productStore';
 import { 
-  View, Text, 
-  StyleSheet, } from 'react-native';
+  View, StyleSheet, 
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
-  const [featuredProducts, setFeaturedProducts] = useState([]);
+  // State local pour stocker les "produits en vedette"
+  const [featuredProducts, setFeaturedProducts] = useState<Product[]>([]);
+  // Extraction des données et méthodes depuis le store Zustand
+  const {
+    products, categories,
+    fetchProducts, fetchCategories,
+    loading, error,
+  } = useProductStore();
+  // Premier effet : chargement des produits et catégories à l'ouverture de l'écran
+  useEffect(() => {
+    fetchProducts();
+    fetchCategories();
+  }, []);
+  // Deuxième effet : sélection de produits "en vedette" quand products change
+  useEffect(() => {
+    // Si la liste des produits n'est pas vide
+    if (products.length > 0) {
+      // Crée une copie inversée des produits (pour simuler une sélection récente en tête de liste)
+      const reverseProducts = [...products].reverse();
+      // Met à jour le state local "featuredProducts"
+      setFeaturedProducts(reverseProducts as Product[]);
+    }
+ }, [products]);
+
   return (
     <View style={styles.wrapper}>
       <HomeHeader />
