e4ab3b106033dd07602bb8d211080137406a0a6a
devJeremie <chabanaisjeremie@gmail.com>
2025-10-10 08:32:42 +0200

    Commetn-LoadingSpinner

--- frontend/components/LoadingSpinner.tsx
+++ frontend/components/LoadingSpinner.tsx
@@ -4,20 +4,21 @@ import {
     View } from 'react-native'
 import React from 'react'
 import { AppColors } from '@/constants/theme';
-
+// Déclaration de l'interface des props, pour typer les arguments du composant
 interface LoadingSpinnerProps {
     size?: "small" | "large";
     color?: string;
     text?: string;
     fullScreen?: boolean;
 }
-
+// Définition du composant fonctionnel
 const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({
     size = "small",
     color = AppColors.primary[500],
     text = "Loading...",
     fullScreen= false,
 }) => {
+    // Si "fullScreen" est true, on retourne un spinner dans une View stylée
     if (fullScreen) {
         return(
         <View style={styles.fullScreen}>
@@ -26,11 +27,12 @@ const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({
         </View>
         );
     }
-  return (
-    <View style={styles.container}>
-        <ActivityIndicator size={size} color={color} />
-        {text && <Text style={styles.text}>{text}</Text>}
-    </View>
+    // Sinon, on retourne le spinner dans une View "container"
+    return (
+        <View style={styles.container}>
+            <ActivityIndicator size={size} color={color} />
+            {text && <Text style={styles.text}>{text}</Text>}
+        </View>
   )
 }
 
