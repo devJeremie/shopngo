e0a2d65c93aca8ef73a38fe7fccbe9e9175d2680
devJeremie <chabanaisjeremie@gmail.com>
2025-10-14 13:41:14 +0200

    CommonHeader-start

--- frontend/app/(tabs)/product/[id].tsx
+++ frontend/app/(tabs)/product/[id].tsx
@@ -1,7 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams } from 'expo-router';
 
 const SingleProductScreen = () => {
+
+    const {id} = useLocalSearchParams<{ id: string }>();
+    console.log("id", id);
+
   return (
     <View>
       <Text>SingleProductScreen</Text>
