e6edbc4a7409ef27d0de87a1660170fe74fff925
devJeremie <chabanaisjeremie@gmail.com>
2025-10-20 16:10:18 +0200

    Shop-Maj

--- frontend/app/(tabs)/shop.tsx
+++ frontend/app/(tabs)/shop.tsx
@@ -1,10 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { AppColors } from '@/constants/theme';
+import Wrapper from '@/components/Wrapper';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
 
 
 const ShopScreen = () => {
   const [products, setProducts] = useState([]);
+  const router = useRouter();
   useEffect(() => {
     const getProducts = async() =>{
       const response = await fetch("https://fakestoreapi.com/products", { 
@@ -19,16 +24,185 @@ const ShopScreen = () => {
     };
     getProducts();
   }, []);
+
+  const renderHeader = () => {
+    return (
+      <View style={styles.header}>
+        <Text style={styles.title}>Tous les produits</Text>
+        <TouchableOpacity 
+          style={styles.searchRow}
+          onPress={() => router.push("/(tabs)/search")}
+        >
+          <View style={styles.searchContainer}>
+            <View style={styles.searchInput}>
+              <Text>Cherchez un produit...</Text>
+            </View>
+          </View>
+          <View 
+            style={styles.searchButton}
+            
+          >
+            <Ionicons 
+              name="search"
+              size={20}
+              color="white"
+            />
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  };
+
   return (
-    <SafeAreaView>
-        <View>
-            <Text>ShopScreen</Text>
-        </View>
-    </SafeAreaView>
-   
+   <Wrapper>
+      {renderHeader()}
+   </Wrapper>
   )
 }
 
+
 export default ShopScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  header: {
+    marginTop: Platform.OS === "android" ? 30 : 0,
+    paddingBottom: 16,
+    backgroundColor: AppColors.background.primary,
+    borderBottomWidth: 1,
+    borderBottomColor: AppColors.gray[200],
+  },
+  title: {
+    fontFamily: 'Inter-Bold',
+    fontSize: 24,
+    color: AppColors.text.primary,
+    marginBottom: 8,
+  },
+  searchRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  searchContainer: {
+    flex: 1,
+  },
+  searchInput: {
+    backgroundColor: AppColors.background.secondary,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    fontSize: 16,
+    borderWidth: 1,
+    borderColor: AppColors.gray[300],
+    color: AppColors.text.primary,
+  },
+  searchInputStyle: {
+    backgroundColor: AppColors.background.secondary,
+    borderRadius: 8,
+    borderColor: "transparent",
+  },
+  searchButton: {
+    backgroundColor: AppColors.primary[500],
+    borderTopRightRadius: 8,
+    borderBottomRightRadius: 8,
+    width: 44,
+    height: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 8,
+    position: "absolute",
+    right: 0, 
+  },
+  sortButton: {
+    backgroundColor: AppColors.background.secondary,
+    borderRadius: 8,
+    width: 44,
+    height: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 8,
+  },
+  activeSortButton: {
+    borderWidth: 1,
+    borderColor: AppColors.error,
+  },
+  categoriesContainer: {
+    paddingVertical: 8,
+  },
+  categoryButton: {
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderRadius: 20,
+    backgroundColor: AppColors.background.secondary,
+    marginRight: 8,
+  },
+  selectedCategory: {
+    backgroundColor: AppColors.primary[500],
+  },
+  categoryText: {
+    fontFamily:"Inter-Medium",
+    fontSize: 14,
+    color: AppColors.text.primary,
+  },
+  selectedCategoryText: {
+    color: "white",
+  },
+  productsGrid: {
+    paddingHorizontal: 5,
+    paddingTop: 16,
+  },
+  columnWrapper: {
+    justifyContent: "space-between",
+  },
+  productContainer: {
+    width:'48%',
+  },
+  footer: {
+    height: 100,
+  },
+  modalOverlay:{
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)"
+  },
+  modalContent: {
+    backgroundColor: AppColors.background.primary,
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    padding: 24,
+  },
+  modalHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 16,
+   },
+  modalTitle: {
+    fontFamily: "Inter-SemiBold",
+    fontSize: 18,
+    color: AppColors.text.primary,
+  },
+  sortOption: {
+    paddingVertical: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: AppColors.gray[200],
+  },
+  activeSortOption: {
+    backgroundColor: AppColors.background.secondary
+  },
+  sortOptionText: {
+    fontFamily: "Inter-Regular",
+    fontSize: 16,
+    color: AppColors.text.primary,
+  },
+  errorContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: "center",
+    padding: 24,
+  },
+  errorText: {
+    fontFamily: "Inter-Medium",
+    fontSize: 16,
+    color: AppColors.error,
+    textAlign: "center",
+  },
+})
