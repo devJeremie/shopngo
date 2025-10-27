6d4a7ba76c06ba7a1327d7b1711582391d6ef35b
devJeremie <chabanaisjeremie@gmail.com>
2025-10-27 08:55:55 +0100

    End-FavoriteStore

--- frontend/store/favoriteStore.ts
+++ frontend/store/favoriteStore.ts
@@ -1,9 +1,9 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
-import { createJSONStorage } from "zustand/middleware";
-import { Product } from "../lib/api";
+import { createJSONStorage, persist } from "zustand/middleware";
+import { Product } from "@/type";
 
-interface FavoriteState {
+interface FavoritesState {
     favoriteItems: Product[];
     addFavorite: (product: Product) => void;
     removeFavorite: (productId: number) => void;
@@ -12,6 +12,41 @@ interface FavoriteState {
     resetFavorite: () => void;
 }
 
-export const useFavoriteStore = create<FavoriteState>()(
-    
+export const useFavoritesStore = create<FavoritesState>()(
+    persist(
+        (set, get) => ({
+            favoriteItems: [],
+
+            addFavorite: (product: Product) => {
+                set((state) => ({
+                    favoriteItems: [...state.favoriteItems, product],
+                }));
+            },
+            removeFavorite: (productId: number) => {
+                set((state) => ({
+                    favoriteItems: state.favoriteItems.filter(
+                        (item) => item.id !== productId
+                    ),
+                }));
+            },
+            resetFavorite: () => {
+                set({ favoriteItems: [] });
+            },
+            toggleFavorite: (product: Product) => {
+                const isFav = get().isFavorite(product.id);
+                if (isFav) {
+                    get().removeFavorite(product.id);
+                } else {
+                    get().addFavorite(product);
+                }
+            },
+            isFavorite: (productId: number) => {
+                return get().favoriteItems.some((item) => item.id === productId );
+            },
+        }),
+        {
+            name: "favorites-storage",
+            storage: createJSONStorage(() => AsyncStorage),
+        }
+    )
 )
