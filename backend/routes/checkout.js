fdaae29cca1e936c6c596e98d9586a450ba381a9
devJeremie <chabanaisjeremie@gmail.com>
2025-10-30 15:13:40 +0100

    Maj-Checkout

--- backend/routes/checkout.js
+++ backend/routes/checkout.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import stripe from "../lib/stripe.js";
 
 const router = Router()
 
@@ -17,9 +18,42 @@ router.post("/checkout", async (req, res) => {
     //Convertir un prix en cents et s'assurer que c'est un entier valide
     const amountInCents = Math.round(price * 100);
     try {
-        
+       const customer = await stripe.customers.create();
+       const ephemeralKey = await stripe.ephemeralKeys.create(
+        {
+            customer: customer.id,
+        },
+        {
+            apiVersion: "2025-09-30.clover"
+        }
+    );
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: amountInCents,
+        currency: "EUR",
+        customer: customer.id,
+        automatic_payment_methods: {
+            enabled: true,
+        },
+        receipt_email: email,
+        description: `Commande de ${email}`,
+        metadata: {
+            email: email,
+        }
+    });
+
+    return res.status(200).send({
+        success: true,
+        message: 'Session de paiement crée avec succés !',
+        paymentIntent: paymentIntent.client_secret,
+        ephemeralKey: ephemeralKey.secret,
+        customer: customer.id,
+    });
     } catch (error) {
-        
+        console.log("Error:", error);
+        return res.status(500).send({
+            success: false,
+            message: "Paiement echoué",
+        });
     }
 });
 
