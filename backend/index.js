3be9d22745b0048883a078ce294c42d68f4af379
devJeremie <chabanaisjeremie@gmail.com>
2025-10-30 14:34:43 +0100

    Maj-.env-Checkout

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import checkout from "./routes/checkout.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -10,6 +11,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("eheh on est bon");
 });
+app.use("/",checkout)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- backend/routes/checkout.js
+++ backend/routes/checkout.js
@@ -1,9 +1,26 @@
-import { Router, router } from "express";
+import { Router } from "express";
 
 const router = Router()
 
-router.get("/checkout", async (req, res) => {
-    res.get("Checkout route is working");
+router.post("/checkout", async (req, res) => {
+    const reqBody = await req.body;
+    const { email, price } = reqBody;
+
+    // Vérifier que le prix est un nombre valide et le convertir en centimes entiers
+    if(typeof price !== "number" || isNaN(price) || price <=0) {
+        return res.status(400).send({
+            success: false,
+            message: "Valeur de prix invalide",
+        });
+    }
+
+    //Convertir un prix en cents et s'assurer que c'est un entier valide
+    const amountInCents = Math.round(price * 100);
+    try {
+        
+    } catch (error) {
+        
+    }
 });
 
 export default router; 
