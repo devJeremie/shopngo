678cc18fbbe00c914625bedf7c00e5be36efefb4
devJeremie <chabanaisjeremie@gmail.com>
2025-11-03 10:52:00 +0100

    MAj-StripePayment

--- frontend/components/StripePayment.tsx
+++ frontend/components/StripePayment.tsx
@@ -1,9 +1,12 @@
 import { 
+  Alert,
     StyleSheet, Text, 
     View 
 } from 'react-native'
 import React from 'react';
 import { useStripe } from "@stripe/stripe-react-native";
+import { useRouter } from 'expo-router';
+import { supabase } from '@/lib/supabase';
 
 type Props = {
   paymentIntent: string;
@@ -19,12 +22,61 @@ const StripePayment = ({
   customer, orderId,
   userEmail, onSuccess
 }: Props) => {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const router = useRouter();
+  //Initialize payment sheet
+  const initializePaymentSheet = async () => {
+    const { error } = await initPaymentSheet({
+      paymentIntentClientSecret: paymentIntent,
+      customerId: customer,
+      customerEphemeralKeySecret: ephemeralKey,
+      merchantDisplayName: 'Shopngo Store',
+    });
+    if (error) {
+      throw new Error(`Échec de l’initialisation de la feuille de paiement: ${error}`);
+    }
+
+    const updatePaymentStatus = async () =>{
+      const {error} =  await supabase
+        .from("orders")
+        .update({ payment_status: "succes" })
+        .eq("id", orderId)
+        .select();
+
+      if (error) {
+        throw new Error(`Échec de l’initialisation de la feuille de paiement: ${error}`);
+      }
+    };
+
+    const handlePayment = async () => {
+      try {
+        await initializePaymentSheet()
+        const { error: presentError } = await presentPaymentSheet();
+        if(presentError) {
+          throw new Error(`Paiement échoué: ${presentError.message}`);
+        }
+        await updatePaymentStatus();
+        Alert.alert("Paiement réussi!", "Merci pour votre commande",[
+          {
+            text: "OK",
+            onPress: () => {
+              onSuccess?.() || router.push("/(tabs)/orders");
+            },
+          },
+        ]);
+      } catch (error) {
+        Alert.alert("Paiment échoué");
+        console.log("Paiment échoué erreur:", error);
+        
+      }
+    };
+  };
   return (
     <View>
       <Text>StripePayment</Text>
     </View>
-  )
-}
+  );
+};
 
 export default StripePayment
 
