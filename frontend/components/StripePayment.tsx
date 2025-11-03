8959110661d78b65be18fe81f1822f363a0f32ac
devJeremie <chabanaisjeremie@gmail.com>
2025-11-03 09:38:55 +0100

    Maj-StripePayment

--- frontend/components/StripePayment.tsx
+++ frontend/components/StripePayment.tsx
@@ -1,7 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { 
+    StyleSheet, Text, 
+    View 
+} from 'react-native'
+import React from 'react';
+import { useStripe } from "@stripe/stripe-react-native";
 
-const StripePayment = () => {
+type Props = {
+  paymentIntent: string;
+  ephemeralKey: string;
+  customer: string;
+  orderId: string; 
+  userEmail: string;
+  onSuccess?: () => void;
+}
+
+const StripePayment = ({
+  paymentIntent, ephemeralKey,
+  customer, orderId,
+  userEmail, onSuccess
+}: Props) => {
   return (
     <View>
       <Text>StripePayment</Text>
