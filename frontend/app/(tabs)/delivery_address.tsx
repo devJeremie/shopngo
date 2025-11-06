0a91a6d5195c3c66e35ada1a4a07258174054f67
devJeremie <chabanaisjeremie@gmail.com>
2025-11-06 11:16:13 +0100

    Start-DeliveryAdress

---
+++ frontend/app/(tabs)/delivery_address.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const delivery_address = () => {
+  return (
+    <View>
+      <Text>delivery_address</Text>
+    </View>
+  )
+}
+
+export default delivery_address
+
+const styles = StyleSheet.create({})
